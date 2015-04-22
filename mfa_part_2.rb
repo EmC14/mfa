e5e1bd8ace60bee6bf2a2ac4eb4cde2e6aa2e81b
EmC14 <ec1003288@students.westport.k12.ct.us>
2015-04-22 17:18:32 -0400

    second commit

--- mfa_part_2.rb
+++ mfa_part_2.rb
@@ -17,29 +17,28 @@ else
   password = 'password'
   hashed_password = Digest::MD5.hexdigest(password_recieved)
   final_password = record[password]
-
-  if final_password == !hashed_password
+  if final_password != hashed_password
     puts "invalid password"
     exit
-    else
-puts "favorite color?"
-    color_recieved = gets.chomp
-    favorite_color = 'favorite color'
+  else
+    puts "favorite color?"
+    color_got = gets.chomp
+    favorite_color = 'favorite_color'
     color = record[favorite_color]
-    if color == !color_recieved
+    if color != color_got
       puts "invalid color"
       exit
     else
       puts "favorite animal?"
       animal_recieved = gets.chomp
-      favorite_animal = 'favorite animal'
+      favorite_animal = 'favorite_animal'
       animal = record[favorite_animal]
-      if animal == !animal_recieved
+      if animal != animal_recieved
         puts "invalid animal"
         exit
       else
         puts "User Authenticated!"
       end
     end
-end
+  end
 end
